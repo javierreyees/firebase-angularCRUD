963c11cd86b8a0cf6a0a17b2959be01472aa5ea4
carlos.reyes.ramirez <javierreyeoc@gmail.com>
2018-10-08 12:21:44 -0500

    remove unused import

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
 
-import { environment } from '../../environments/environment';
-
 @Injectable()
 export class CarService {
 
